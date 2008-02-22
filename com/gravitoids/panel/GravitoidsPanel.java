b3a03f01665730fd5bfe6f32f689d117a89854b0
Michael Cook <foobarsoft@foobarsoft.com>
2008-02-22 14:37:37 +0000

    Changed file output path, added BSD licenses to all files.

--- com/gravitoids/panel/GravitoidsPanel.java
+++ com/gravitoids/panel/GravitoidsPanel.java
@@ -26,6 +26,33 @@ import com.gravitoids.bean.IntelligentGravitoidsShip;
 import com.gravitoids.helper.GravityHelper;
 import com.gravitoids.main.GravitoidsGame;
 
+/**
+ * Copyright (c) 2008, Michael Cook
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Michael Cook nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Cook ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Michael Cook BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 public class GravitoidsPanel extends JPanel implements Runnable, KeyListener {
 	public static final int PANEL_WIDTH = 640;
 	public static final int PANEL_HEIGHT = 480; 
@@ -401,8 +428,7 @@ public class GravitoidsPanel extends JPanel implements Runnable, KeyListener {
 	
 	private void saveDeadShips() {
 		try {
-			//FileWriter fw = new FileWriter("/Users/michael/desktop/graivtoids_ships.dat");
-			FileWriter fw = new FileWriter("c:\\graivtoids_ships.dat");
+			FileWriter fw = new FileWriter("graivtoids_ships.dat");
 			
 			fw.append("Generation: " + generation + "\n");
 			
