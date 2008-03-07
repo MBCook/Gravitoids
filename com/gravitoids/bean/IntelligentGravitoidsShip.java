e9f22ad84c14f40171ff21c9896e20a63a2d19c4
Michael Cook <foobarsoft@foobarsoft.com>
2008-03-07 22:01:31 +0000

    Cleaned up the logic into more functions, changed the way minimum fear is used so that they actually have a chance to obey it, took out a unneccessary calculation that would slow things down and be easy to mis-maintain, changed the way we calculate what we're affraid of in an attempt to be more robust, changed it so the object that we are more afraid of always gets more thrust.

--- com/gravitoids/bean/IntelligentGravitoidsShip.java
+++ com/gravitoids/bean/IntelligentGravitoidsShip.java
@@ -165,80 +165,66 @@ public class IntelligentGravitoidsShip extends GravitoidsAutonomousObject {
 				brain[cTerm];
 	}
 	
-	public void prepareMove(GravitoidsObject[] theirObjects) {
-		// Look at each object, decide how much we care, and how we will respond
-		// Then add it all together
+	private void asessObject(GravitoidsObject object, double[] weights, double[] headings, int index) {
+		// Figure out our distance from it, used in the gravity calculation
 		
-		GravitoidsObject[] objects = new GravitoidsObject[theirObjects.length + 2];	// 2 more so we can track walls
+		double distance = Math.sqrt(Math.pow(getXPosition() - object.getXPosition(), 2) + 
+										Math.pow(getYPosition() - object.getYPosition(), 2));
 		
-		double[] weights = new double[objects.length];	// How much we care
-		double[] headings = new double[objects.length];	// Which heading to go in
+		// Now handle it's mass to get gravity
 		
-		for (int i = 0; i < objects.length - 2; i++) {
-			// Get what we're working with
-			
-			GravitoidsObject object = theirObjects[i];
-			
-			objects[i] = object;
-			
-			// Figure out our distance from it, used in the gravity calculation
-			
-			double distance = Math.sqrt(Math.pow(getXPosition() - object.getXPosition(), 2) + 
-											Math.pow(getYPosition() - object.getYPosition(), 2));
-			
-			// Now handle it's mass to get gravity
-			
-			double gravity = calculateFromBrain(distance / object.getMass(), OBJECT_GRAVITY_A_TERM, OBJECT_GRAVITY_B_TERM, OBJECT_GRAVITY_C_TERM);
-			
-			// Now the direction
-			
-			double direction = Math.atan((getXPosition() - object.getXPosition()) / 
-											(getYPosition() - object.getYPosition()));
-			
-			direction = calculateFromBrain(direction, OBJECT_DIRECTION_A_TERM, OBJECT_DIRECTION_B_TERM, OBJECT_DIRECTION_C_TERM);
-			
-			while (direction > 2.0 * Math.PI) {	// Clamp it
-				direction -= 2.0 * Math.PI;
-			}
-			
-			while (direction < 0.0) {
-				direction += 2.0 * Math.PI;
-			}
-			
-			// The object's speed relative to us
-			
-			double xSpeedDelta = getXSpeed() - object.getXSpeed();
-			double ySpeedDelta = getYSpeed() - object.getYSpeed();
-			
-			if ((xSpeedDelta < 0.0) && (getXPosition() > object.getXPosition())) {	// Copensate for relative positions
-				xSpeedDelta *= -1.0;
-			}
-			
-			if ((ySpeedDelta < 0.0) && (getYPosition() > object.getYPosition())) {
-				ySpeedDelta *= -1.0;
-			}
-			
-			xSpeedDelta = xSpeedDelta < 0.0 ? 0.0 : xSpeedDelta;	// Mark we don't care about negative speeds
-			ySpeedDelta = ySpeedDelta < 0.0 ? 0.0 : ySpeedDelta;
-			
-			double speed = Math.sqrt(xSpeedDelta * xSpeedDelta + ySpeedDelta * ySpeedDelta);
-			
-			speed = calculateFromBrain(speed, OBJECT_SPEED_A_TERM, OBJECT_SPEED_B_TERM, OBJECT_SPEED_C_TERM);
-			
-			// Now the object's size
-			
-			double size = calculateFromBrain(object.getRadius(), OBJECT_SIZE_A_TERM, OBJECT_SIZE_B_TERM, OBJECT_SIZE_C_TERM);
-			
-			// Now moveability
-			
-			double moveability = object.isMoveable() ? brain[OBJECT_MOVEABLE_FACTOR] : 0.0;
-			
-			// Store things we need to for this object
-			
-			weights[i] = moveability + gravity + size + speed;
-			headings[i] = direction;
+		double gravity = calculateFromBrain(distance / object.getMass(), OBJECT_GRAVITY_A_TERM, OBJECT_GRAVITY_B_TERM, OBJECT_GRAVITY_C_TERM);
+		
+		// Now the direction
+		
+		double direction = Math.atan((getXPosition() - object.getXPosition()) / 
+										(getYPosition() - object.getYPosition()));
+		
+		direction = calculateFromBrain(direction, OBJECT_DIRECTION_A_TERM, OBJECT_DIRECTION_B_TERM, OBJECT_DIRECTION_C_TERM);
+		
+		while (direction > 2.0 * Math.PI) {	// Clamp it
+			direction -= 2.0 * Math.PI;
 		}
 		
+		while (direction < 0.0) {
+			direction += 2.0 * Math.PI;
+		}
+		
+		// The object's speed relative to us
+		
+		double xSpeedDelta = getXSpeed() - object.getXSpeed();
+		double ySpeedDelta = getYSpeed() - object.getYSpeed();
+		
+		if ((xSpeedDelta < 0.0) && (getXPosition() > object.getXPosition())) {	// Copensate for relative positions
+			xSpeedDelta *= -1.0;
+		}
+		
+		if ((ySpeedDelta < 0.0) && (getYPosition() > object.getYPosition())) {
+			ySpeedDelta *= -1.0;
+		}
+		
+		xSpeedDelta = xSpeedDelta < 0.0 ? 0.0 : xSpeedDelta;	// Mark we don't care about negative speeds
+		ySpeedDelta = ySpeedDelta < 0.0 ? 0.0 : ySpeedDelta;
+		
+		double speed = Math.sqrt(xSpeedDelta * xSpeedDelta + ySpeedDelta * ySpeedDelta);
+		
+		speed = calculateFromBrain(speed, OBJECT_SPEED_A_TERM, OBJECT_SPEED_B_TERM, OBJECT_SPEED_C_TERM);
+		
+		// Now the object's size
+		
+		double size = calculateFromBrain(object.getRadius(), OBJECT_SIZE_A_TERM, OBJECT_SIZE_B_TERM, OBJECT_SIZE_C_TERM);
+		
+		// Now moveability
+		
+		double moveability = object.isMoveable() ? brain[OBJECT_MOVEABLE_FACTOR] : 0.0;
+		
+		// Store things we need to for this object
+		
+		weights[index] = moveability + gravity + size + speed;
+		headings[index] = direction;
+	}
+	
+	private void assessWalls(GravitoidsObject[] objects, double[] weights, double[] headings) {
 		// Now, we'll insert the closest X and Y walls, with apropriate weights
 		
 		GravitoidsObject sideWall = new WallObject();
@@ -310,72 +296,96 @@ public class IntelligentGravitoidsShip extends GravitoidsAutonomousObject {
 		}
 		
 		headings[topWallIndex] = direction;
+	}
+	
+	public void prepareMove(GravitoidsObject[] theirObjects) {
+		// Prepare arrays to hold stuff we'll be messing with, and how much we care
+		
+		GravitoidsObject[] objects = new GravitoidsObject[theirObjects.length + 2];	// 2 more so we can track walls
+		
+		double[] weights = new double[objects.length];	// How much we care
+		double[] headings = new double[objects.length];	// Which heading to go in
+		
+		// Assess each object in the universe we are told about
+		
+		for (int i = 0; i < objects.length - 2; i++) {
+			// Get what we're working with
+			
+			GravitoidsObject object = theirObjects[i];
+			
+			objects[i] = object;
+			
+			asessObject(object, weights, headings, i);
+		}
+		
+		// Now assess the closest walls
+		
+		assessWalls(objects, weights, headings);
 		
 		// Now that we have all that, we need to find what we care about most
 		
-		int importantIndex = -1;
-		int secondIndex = -1;
+		int importantIndex = -1;			// Thing we are most scared of
+		int secondImportantIndex = -1;		// Thing we are second most scared of
 		
-		double mostImportant = Double.NEGATIVE_INFINITY;
-		double secondMostImportant = Double.NEGATIVE_INFINITY;
+		double mostImportant = Double.NEGATIVE_INFINITY;		// Known value
+		double secondMostImportant = Double.NEGATIVE_INFINITY;	// Known value
+		
+		double minimumFear = brain[MINIMUM_FEAR] + 1.0 * 10.0;	// 10.0 gives us a range
+		//double minimumFear = 0.0;
 		
 		for (int i = 0; i < weights.length; i++) {
-			if (weights[i] >= mostImportant) {
+			if ((weights[i] >= mostImportant) && (weights[i] > minimumFear)) {
 				secondMostImportant = mostImportant;
 				mostImportant = weights[i];
 				
-				secondIndex = importantIndex;
+				secondImportantIndex = importantIndex;
 				importantIndex = i;
-			} else if (weights[i] >= secondMostImportant) {
+			} else if ((weights[i] >= secondMostImportant) && (weights[i] > minimumFear)) {
 				secondMostImportant = weights[i];
-				secondIndex = i;
+				secondImportantIndex = i;
 			}
-		}
-
-		// Record the position of the object we are being motivated by
-
-		double minimumFear = (1.0 - (brain[MINIMUM_FEAR] + 1.0) / 2.0) * Double.MAX_VALUE * -1.0;
-		
-		if (mostImportant > minimumFear) {
-			motivationX = objects[importantIndex].getXPosition();
-			motivationY = objects[importantIndex].getYPosition();
-		} else {
-			motivationX = -1.0;
-			motivationY = -1.0;
-		}
-		
-		if (secondMostImportant > minimumFear) {
-			otherMotivationX = objects[secondIndex].getXPosition();
-			otherMotivationY = objects[secondIndex].getYPosition();
-		} else {
-			otherMotivationX = -1.0;
-			otherMotivationY = -1.0;
 		}
 		
 		// Now that we have that, we'll scale thrust to the normal of the weights
 
-		if ((importantIndex >= 0) && (mostImportant > minimumFear)) {
-			if ((secondIndex >= 0) && (secondMostImportant > minimumFear)) {
+		if (importantIndex >= 0) {
+			// Setup to draw this object
+			
+			motivationX = objects[importantIndex].getXPosition();
+			motivationY = objects[importantIndex].getYPosition();
+			
+			if (secondImportantIndex >= 0) {
+				// Setup to draw this one too
+				
+				otherMotivationX = objects[secondImportantIndex].getXPosition();
+				otherMotivationY = objects[secondImportantIndex].getYPosition();
+				
 				// OK, two things to handle
 				
 				double newThrustA = weights[importantIndex];
 				
 				newThrustA = calculateFromBrain(newThrustA, CARE_TO_THRUST_A_TERM, CARE_TO_THRUST_B_TERM, CARE_TO_THRUST_C_TERM);
 				
-				double newThrustB = weights[secondIndex];
+				double newThrustB = weights[secondImportantIndex];
 				
 				newThrustB = calculateFromBrain(newThrustB, CARE_TO_THRUST_A_TERM, CARE_TO_THRUST_B_TERM, CARE_TO_THRUST_C_TERM);
 				
-				newThrustA = newThrustA * ((brain[THRUST_SPLIT] + 1.0) / 2.0);
-				newThrustB = newThrustB * (1.0 - ((brain[THRUST_SPLIT] + 1.0) / 2.0));
+				double splitFactor = (brain[THRUST_SPLIT] + 1.0) / 2.0;
+				
+				if (splitFactor < 0.5) {				// Make sure we base thrust more on
+					splitFactor = 1.0 - splitFactor;	//		the object we are more fearful of
+				}
+				
+				newThrustA = newThrustA * splitFactor;
+				newThrustB = newThrustB * (1.0 - splitFactor);	// These are supposed to be compliments
 				
 				// Now we have to add the two thrust vectors
 				
 				double xThrust = Math.cos(headings[importantIndex]) * newThrustA;
 				double yThrust = Math.sin(headings[importantIndex]) * newThrustA;
 				
-				xThrust += Math.cos(headings[secondIndex]) * newThrustB;
-				yThrust += Math.sin(headings[secondIndex]) * newThrustB;
+				xThrust += Math.cos(headings[secondImportantIndex]) * newThrustB;
+				yThrust += Math.sin(headings[secondImportantIndex]) * newThrustB;
 				
 				// Now we have to get that back into a thrust and thrust percentages
 				// Get the total thrust, then normalize the two vector parts
@@ -387,6 +397,11 @@ public class IntelligentGravitoidsShip extends GravitoidsAutonomousObject {
 				setXThrustPortion(xThrust / factor);
 				setYThrustPortion(yThrust / factor);
 			} else {
+				// Don't draw a second object we're affraid of
+				
+				otherMotivationX = -1.0;
+				otherMotivationY = -1.0;
+				
 				// Just one, this is easy
 				
 				double newThrust = weights[importantIndex];
@@ -401,6 +416,13 @@ public class IntelligentGravitoidsShip extends GravitoidsAutonomousObject {
 				setYThrustPortion(Math.sin(headings[importantIndex]));
 			}
 		} else {
+			// Don't draw either object since we're not afraid
+			
+			motivationX = -1.0;
+			motivationY = -1.0;
+			otherMotivationX = -1.0;
+			otherMotivationY = -1.0;
+			
 			// We ain't scared of nothing
 			
 			setThrust(0.0);
