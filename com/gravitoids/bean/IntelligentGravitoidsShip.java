beb908f0f2842a9f4678c8b2aa50115b3bc15747
Michael Cook <foobarsoft@foobarsoft.com>
2008-03-17 14:40:52 +0000

    Nothing of consequence

--- com/gravitoids/bean/IntelligentGravitoidsShip.java
+++ com/gravitoids/bean/IntelligentGravitoidsShip.java
@@ -331,8 +331,7 @@ public class IntelligentGravitoidsShip extends GravitoidsAutonomousObject {
 		double secondMostImportant = Double.NEGATIVE_INFINITY;	// Known value
 		
 		double minimumFear = brain[MINIMUM_FEAR] + 1.0 * 10.0;	// 10.0 gives us a range
-		//double minimumFear = 0.0;
-		
+
 		for (int i = 0; i < weights.length; i++) {
 			if ((weights[i] >= mostImportant) && (weights[i] > minimumFear)) {
 				secondMostImportant = mostImportant;
